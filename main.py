e3e27d59c9244f9d6a463a63931c19cf0122a133
Thomodo <thomodo703@gmail.com>
2026-02-15 00:51:02 +0100

    Choix de charger la sauvegarde ou d'en créer une nouvelle

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import classe
-import function
+#import function
+
+### Initialisaton (on veut savoir si le joueur veut charger la sauvegarde)
+
+ans = input("Voulez vous charger la sauvegarde ?\n"
+            "'O' pour Oui, 'N' pour Non\n"
+            "\n")
+while ans not in ["O","N"]:
+    ans = input("Vous devez répondre 'O' ou 'N'!\n")
+
+
+if ans == "O":
+    print("Chargement de la sauvegarde")
+else:
+    print("Création d'une nouvelle sauvegarde")
