f6c6e3c2a4a09a976f09521f0fae825fd65fd5e0
Thomodo <thomodo703@gmail.com>
2026-03-04 09:30:42 +0100

    finiolage

--- main.py
+++ main.py
@@ -27,25 +27,25 @@ else:
 quit = False 
 
 while quit != True : 
-    choice = input (" Que souhaité vous faire ? \n" 
-                " Pour accéder à votre boutique, entrez 'boutique'\n"
-                " Pour effectuer un combat de boss, entrez 'boss'\n"
-                " Pour effectuer un combat contre un monstre, entrez 'monstre' \n"
-                " Pour quitter la partie , entrez 'quitter' \n")
+    choice = input ("Que souhaité vous faire ? \n" 
+                "Pour accéder à votre boutique, entrez 'boutique'\n"
+                "Pour effectuer un combat de boss, entrez 'boss'\n"
+                "Pour effectuer un combat contre un monstre, entrez 'monstre' \n"
+                "Pour quitter la partie , entrez 'quitter' \n")
     
     while choice not in [ "Boutique","boutique", "Boss", "boss","Monstre","monstre",' Quitter', 'quitter']:
         choice = input("Vous devez répondre par soit par 'boutique' soit par  'boss'soit par  'monstre' ou par 'quitter'!\n")
         
         if choice in ["Boutique","boutique"]:
-             print ( "Bienvenue dans la boutique (pseudo)")
+             print("Bienvenue dans la boutique " + player.player_data_dict["name"])
 
         elif choice in ["Boss","boss"]:
-            print ("Vous vous appretez à effectuer un combat de boss")
+            print("Vous vous appretez à effectuer un combat de boss")
             
         elif choice in ["Monstre","monstre"]:
-            print ("Vous vous appretez à affronter un monstre")
+            print("Vous vous appretez à affronter un monstre")
         
-        elif choice in [ "Quitter", "quitter"]:
-            print (" Vous allez quitter le jeu, votre jeu sera automatiquement sauvegarder ")
+        elif choice in ["Quitter", "quitter"]:
+            print(" Vous allez quitter le jeu, votre jeu sera automatiquement sauvegarder ")
             player.save_player_data()
             quit = True 
