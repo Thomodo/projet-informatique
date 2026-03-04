ad8fde0055fd0d9e41345c4b8c07a9605c0bd982
Mathilde25-hub <mathilde.deluzydepelissac@gmail.com>
2026-03-04 09:21:12 +0100

    J'ai faitb le choix

--- main.py
+++ main.py
@@ -22,3 +22,30 @@ else:
     player.name_choose()
 
 ### choisir ce qu'on fait
+
+### choisir ce qu'on fait
+quit = False 
+
+while quit != True : 
+    choice = input (" Que souhaité vous faire ? \n" 
+                " Pour accéder à votre boutique, entrez 'boutique'\n"
+                " Pour effectuer un combat de boss, entrez 'boss'\n"
+                " Pour effectuer un combat contre un monstre, entrez 'monstre' \n"
+                " Pour quitter la partie , entrez 'quitter' \n")
+    
+    while choice not in [ "Boutique","boutique", "Boss", "boss","Monstre","monstre",' Quitter', 'quitter']:
+        choice = input("Vous devez répondre par soit par 'boutique' soit par  'boss'soit par  'monstre' ou par 'quitter'!\n")
+        
+        if choice in ["Boutique","boutique"]:
+             print ( "Bienvenue dans la boutique (pseudo)")
+
+        elif choice in ["Boss","boss"]:
+            print ("Vous vous appretez à effectuer un combat de boss")
+            
+        elif choice in ["Monstre","monstre"]:
+            print ("Vous vous appretez à affronter un monstre")
+        
+        elif choice in [ "Quitter", "quitter"]:
+            print (" Vous allez quitter le jeu, votre jeu sera automatiquement sauvegarder ")
+            player.save_player_data()
+            quit = True 
